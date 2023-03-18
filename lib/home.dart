ca20cf2231b12540f02b5a6876730164c39a2fd1
Sushobhan Nayak <sushobhannayak17@gmail.com>
2023-03-18 21:49:28 +0530

    First Commit

---
+++ lib/home.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:pothole_detector/map.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(centerTitle: true, title: const Text('Welcome')),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Center(
+              child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const LiveLocationPage()),
+                    );
+                  },
+                  child: const Text(
+                    'Report pothole',
+                    style: TextStyle(fontSize: 20),
+                  )))
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/map.dart
@@ -0,0 +1,190 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:location/location.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+class LiveLocationPage extends StatefulWidget {
+  static const String route = '/live_location';
+
+  const LiveLocationPage({Key? key}) : super(key: key);
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _LiveLocationPageState createState() => _LiveLocationPageState();
+}
+
+class _LiveLocationPageState extends State<LiveLocationPage> {
+  LocationData? _currentLocation;
+  late final MapController _mapController;
+
+  bool _liveUpdate = false;
+  bool _permission = false;
+
+  String? _serviceError = '';
+
+  int interActiveFlags = InteractiveFlag.all;
+
+  final Location _locationService = Location();
+
+  @override
+  void initState() {
+    super.initState();
+    _mapController = MapController();
+    initLocationService();
+  }
+
+  void initLocationService() async {
+    await _locationService.changeSettings(
+      accuracy: LocationAccuracy.high,
+      interval: 1000,
+    );
+
+    LocationData? location;
+    bool serviceEnabled;
+    bool serviceRequestResult;
+
+    try {
+      serviceEnabled = await _locationService.serviceEnabled();
+
+      if (serviceEnabled) {
+        final permission = await _locationService.requestPermission();
+        _permission = permission == PermissionStatus.granted;
+
+        if (_permission) {
+          location = await _locationService.getLocation();
+          _currentLocation = location;
+          _locationService.onLocationChanged
+              .listen((LocationData result) async {
+            if (mounted) {
+              setState(() {
+                _currentLocation = result;
+
+                // If Live Update is enabled, move map center
+                if (_liveUpdate) {
+                  _mapController.move(
+                      LatLng(_currentLocation!.latitude!,
+                          _currentLocation!.longitude!),
+                      _mapController.zoom);
+                }
+              });
+            }
+          });
+        }
+      } else {
+        serviceRequestResult = await _locationService.requestService();
+        if (serviceRequestResult) {
+          initLocationService();
+          return;
+        }
+      }
+    } on PlatformException catch (e) {
+      debugPrint(e.toString());
+      if (e.code == 'PERMISSION_DENIED') {
+        _serviceError = e.message;
+      } else if (e.code == 'SERVICE_STATUS_ERROR') {
+        _serviceError = e.message;
+      }
+      location = null;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    LatLng currentLatLng;
+
+    // Until currentLocation is initially updated, Widget can locate to 0, 0
+    // by default or store previous location value to show.
+    if (_currentLocation != null) {
+      currentLatLng =
+          LatLng(_currentLocation!.latitude!, _currentLocation!.longitude!);
+    } else {
+      currentLatLng = LatLng(0, 0);
+    }
+
+    final markers = <Marker>[
+      Marker(
+        width: 100,
+        height: 100,
+        point: currentLatLng,
+        builder: (context) => const Icon(
+          Icons.location_on,
+          color: Colors.black,
+          size: 50,
+        ),
+      ),
+    ];
+
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home')),
+      // drawer: buildDrawer(context, LiveLocationPage.route),
+      body: Padding(
+        padding: const EdgeInsets.all(8),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 8, bottom: 8),
+              child: _serviceError!.isEmpty
+                  ? Text('Your current location is '
+                      '(${currentLatLng.latitude}, ${currentLatLng.longitude}).')
+                  : Text(
+                      'Error occured while acquiring location. Error Message : '
+                      '$_serviceError'),
+            ),
+            Flexible(
+              child: FlutterMap(
+                mapController: _mapController,
+                options: MapOptions(
+                  center:
+                      LatLng(currentLatLng.latitude, currentLatLng.longitude),
+                  zoom: 16,
+                  interactiveFlags: interActiveFlags,
+                ),
+                children: [
+                  TileLayer(
+                    urlTemplate:
+                        'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                    userAgentPackageName: 'dev.fleaflet.flutter_map.example',
+                  ),
+                  MarkerLayer(markers: markers),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: Builder(builder: (BuildContext context) {
+        return Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ElevatedButton(
+                onPressed: () {}, child: const Text('Send location')),
+            const SizedBox(width: 145),
+            FloatingActionButton(
+              onPressed: () {
+                setState(() {
+                  _liveUpdate = !_liveUpdate;
+
+                  if (_liveUpdate) {
+                    interActiveFlags = InteractiveFlag.rotate |
+                        InteractiveFlag.pinchZoom |
+                        InteractiveFlag.doubleTapZoom |
+                        InteractiveFlag.drag;
+                  } else {
+                    interActiveFlags = InteractiveFlag.all;
+                  }
+                });
+              },
+              child: _liveUpdate
+                  ? const Icon(Icons.location_on)
+                  : const Icon(Icons.location_off),
+            ),
+          ],
+        );
+      }),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+    );
+  }
+}
